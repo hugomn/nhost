f80dd857df9542e761fd604440e813b078f6240f
David BM <correodelnino@gmail.com>
2026-05-04 16:59:58 +0200

    wip: refactor

--- dashboard/src/features/orgs/projects/database/dataGrid/hooks/useCreateComputedFieldMutation/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as useCreateComputedFieldMutation } from './useCreateComputedFieldMutation';
--- dashboard/src/features/orgs/projects/database/dataGrid/hooks/useEditComputedFieldMutation/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as useEditComputedFieldMutation } from './useEditComputedFieldMutation';
