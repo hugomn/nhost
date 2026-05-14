da15f9c60b118163502eb9affd04d96d7237e8c2
robertkasza <robert.kasza@bishop-co.com>
2026-05-14 20:43:02 +0200

    fix(dashboard): Migrate services page icons

--- dashboard/src/features/orgs/projects/services/components/ServiceForm/components/ReplicasFormSection/ReplicasFormSection.tsx
+++ dashboard/src/features/orgs/projects/services/components/ServiceForm/components/ReplicasFormSection/ReplicasFormSection.tsx
@@ -1,9 +1,8 @@
+import { InfoIcon, InfoIcon as InfoOutlinedIcon } from 'lucide-react';
 import { useState } from 'react';
 import { useFormContext, useWatch } from 'react-hook-form';
 import { Box } from '@/components/ui/v2/Box';
 import { Input } from '@/components/ui/v2/Input';
-import { InfoIcon } from '@/components/ui/v2/icons/InfoIcon';
-import { InfoOutlinedIcon } from '@/components/ui/v2/icons/InfoOutlinedIcon';
 import { Switch } from '@/components/ui/v2/Switch';
 import { Text } from '@/components/ui/v2/Text';
 import { Tooltip } from '@/components/ui/v2/Tooltip';
@@ -65,7 +64,7 @@ export default function ReplicasFormSection() {
             </Text>
           }
         >
-          <InfoIcon aria-label="Info" className="h-4 w-4" color="primary" />
+          <InfoIcon aria-label="Info" className="h-4 w-4 text-primary" />
         </Tooltip>
       </Box>
 
